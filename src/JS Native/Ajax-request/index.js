52cbfe65d8edef75b9b0b6deb97f4726463dddea
Yujik <yujika87@gmail.com>
2022-02-07 22:22:42 +0300

    Ajax request

--- src/JS Native/Ajax-request/index.js
+++ src/JS Native/Ajax-request/index.js
@@ -1,18 +1,13 @@
 const imgNumber = document.querySelector('#img-number');
 const showMeButton = document.querySelector('#show-me');
-showMeButton.addEventListener('click', makeRequest);
+showMeButton.addEventListener('click', () => {
+    const promise = getImages(imgNumber.value)
+});
 
-
-function makeRequest() {
-    $.ajax(`https://repetitora.net/api/JS/Images?page=${imgNumber.value}&count=3`, {
-        success: function (data) {
-            data.forEach(el => {
-                const img = document.createElement('img');
-                img.src = el.thumbnail;
-                document.querySelector('#result').appendChild(img);
-            })
-        }
-    });
+function onDataRecieved (data) {
+    data.forEach(el => {
+        const img = document.createElement('img');
+        img.src = el.thumbnail;
+        document.querySelector('#result').appendChild(img);
+    })
 }
-
-console.log(1)
